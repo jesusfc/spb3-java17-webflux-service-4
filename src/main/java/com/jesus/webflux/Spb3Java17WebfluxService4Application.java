4693f76a98baa804851a6addbf56ada6d3099f9e
Jesusfc <jfcaraballo@gmail.com>
2025-04-14 16:19:06 +0200

    update First commit

---
+++ src/main/java/com/jesus/webflux/Spb3Java17WebfluxService4Application.java
@@ -0,0 +1,13 @@
+package com.jesus.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb3Java17WebfluxService4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spb3Java17WebfluxService4Application.class, args);
+    }
+
+}
